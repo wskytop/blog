df37dd7b3ae0cfcb5cc1a7869d44eb63e7d4bae3
wskytop <wskytop@163.com>
2022-08-16 14:22:45 +0800

    feat:配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  server: {
+    port: 8080, //启动端口
+    hmr: {
+      host: '127.0.0.1',
+      port: 8080
+    },
+  },
+  optimizeDeps: {
+    include: [
+      "vue",
+      "vue-router",
+    ],
+  },
 })

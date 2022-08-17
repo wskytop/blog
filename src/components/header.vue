3d013cef8420287c9cd197e52d0c2c36cd58c47c
wskytop <wskytop@163.com>
2022-08-17 11:04:25 +0800

    feat:头部导航

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="header flex-row-b">
-    <div class="header-title">
-      <div>daydayup</div>
+  <div class="header" :style="{ backgroundImage: 'url(' + author.bgImg + ')', backgroundSize: 'cover' }">
+    <div class="banner flex-row-b">
+      <div class="header-title">
+        <div>daydayup</div>
+      </div>
+      <div class="tab-list flex">
+        <div class="tab pointer" @click="goFont">首页</div>
+        <div class="tab pointer" @click="goAbout">关于</div>
+      </div>
     </div>
-    <div class="banner flex">
-      <div class="tab pointer" @click="goFont">首页</div>
-      <div class="tab pointer" @click="goAbout">关于</div>
+    <div class="introduction flex-col-a">
+      <div class="avatar">
+        <el-avatar :size="128" :src="author.avatar" />
+      </div>
+      <div class="title">{{ author.name }}</div>
+      <div class="description">{{ author.description }}</div>
+      <!-- <div class="myOthers"></div> -->
     </div>
   </div>
 </template>
 <script setup>
 import { useRouter } from "vue-router"
+import { ref } from 'vue'
+import { kyt, pmh } from '~/config/author'
 const $router = useRouter()
+const author = ref({})
+author.value = kyt
+console.log(author.value);
 const goFont = () => {
   $router.push('/')
 }
@@ -22,29 +37,55 @@ const goAbout = () => {
 
 <style lang="scss" scoped>
 .header {
-  box-shadow: 0 0.1rem 0.1rem rgba(0, 0, 0, 0.112);
-  width: 100vw;
-  height: 4.5rem;
-  background-color: transparent;
+  height: 42rem;
 
-  &-title {
-    margin-left: 10%;
-    font-weight: bold;
-    font-size: 1.8rem;
-    transition: 0.2s;
+  .banner {
+    box-shadow: 0 0.1rem 0.1rem rgba(0, 0, 0, 0.112);
+    width: 100vw;
+    height: 4.5rem;
+    background-color: transparent;
 
-    &:hover {
-      letter-spacing: 0.2rem;
+    .header-title {
+      color: white;
+      margin-left: 10%;
+      font-weight: bold;
+      font-size: 1.8rem;
+      transition: 0.2s;
+
+      &:hover {
+        letter-spacing: 0.2rem;
+      }
+    }
+
+    .tab-list {
+      color: white;
+      font-size: 1.6rem;
+      width: 20rem;
+      margin-right: 10%;
+
+      .tab {
+        margin-right: 1rem;
+      }
     }
   }
 
-  .banner {
-    font-size: 1.6rem;
-    width: 20rem;
-    margin-right: 10%;
+  .introduction {
+    height: 25rem;
 
-    .tab {
-      margin-right: 1rem;
+    .title {
+      font-size: 4.4rem;
+      font-weight: bold;
+      color: white;
+      transition: 0.2s;
+
+      &:hover {
+        letter-spacing: 0.4rem;
+      }
+    }
+
+    .description {
+      font-size: 2.4rem;
+      color: #666666;
     }
   }
 }
---
+++ src/config/author.ts
@@ -0,0 +1,15 @@
+
+export const kyt = {
+  avatar: 'https://img2.baidu.com/it/u=3678312618,1363990359&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=400',
+  name: "Hugh's blog",
+  description: '专注web开发的网站',
+  others: '',
+  bgImg: '../../public/imgs/banner.jpg'
+}
+export const pmh = {
+  avatar: 'https://img2.baidu.com/it/u=2482126084,3566358098&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
+  name: "Ttods's blog",
+  description: '专注web开发的网站',
+  others: '',
+  bgImg: ''
+}

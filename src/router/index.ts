905746e02a1255939f305eb550691600c4c67c7e
wskytop <wskytop@163.com>
2022-12-14 14:31:07 +0800

    feat:目录重构

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from '~pages'
 
-routes[0].path = '/w-blog'
 const router = createRouter({
   history: createWebHistory(),
   routes,

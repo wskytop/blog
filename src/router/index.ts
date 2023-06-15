31bc7e0553dadcb0895daa81cf378c12b6a8e8e3
wskytop <wskytop@163.com>
2023-06-15 17:44:53 +0800

    fix: 路由模式更换

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from '~pages'
 
 const router = createRouter({
-  history: createWebHistory('/w-blog/'),
+  history: createWebHashHistory('/w-blog/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     // 路由跳转时定位到顶部
